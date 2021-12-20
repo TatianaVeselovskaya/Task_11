8f46600183d751b3e7d70d642a00a2eadaa9dbd5
Татьяна <veselovskayt@mail.ru>
2021-12-20 18:58:10 +0700

    Добавьте файлы проекта.

---
+++ Task_11/Line_yrav.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_11
+{
+    struct Line_yrav
+    {
+        double k;
+        double b;
+
+        public Line_yrav (double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public string Root()
+        {
+            double X = b / k;
+            if (k == 0)
+              return "Нет решений";
+            return $"Ответ: х = {X}";
+        }
+    }
+}
---
+++ Task_11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Для решения линейного уравнения 0=kx+b");
+            Console.Write("Введите коэффициент к= ");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите свободный член b= ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Line_yrav line_yrav = new Line_yrav(k, b);
+            Console.WriteLine(line_yrav.Root());
+            Console.ReadKey();
+        }
+    }
+}
